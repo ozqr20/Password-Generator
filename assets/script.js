f2ff0bb6b3d66fdd1fd503a563eb703339101a6d
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-06-27 20:52:44 -0400

    href for css and js added

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
 // Assignment code here
 
+var password = function generatePassword(){
+
+
+
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
