c044d41e76d11772f8dacfb71dcdb48255f10a2b
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-06-27 22:28:34 -0400

    pseudo code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
+/* PSEUDO CODE
+
+  1. Button to generate password should call the function to generate the password 
+  2. Create an object call userChoice
+  3. Then a prompt that will display the password criteria, the variables must be bolean type yes or not to be able to check inside the functions 
+      we use var passWordLenght = prompt("Message 1 lenght of the password ") and save it into a variable call passWordLenght
+      we use var lowerCase = prompt("Message 2 lowercase ") and save it into a variable call lowerCase
+      we use var upperCase = prompt("Message 3 uppercase ") and save it into a variable call upperCase
+      we use var numericValue = prompt("Message 4 numeric ") and save it into a variable call numericValue
+      we use var specialCharacters = console.prompt("Message 5 &&/|| special characters ") and save it into a variable call specialCharacters 
+      check using if statement if the input is valid 
+  
+  4. According to the user selection a switch statement would fit 
+      - switch statement 
+        - option 1 will call the function the lenght of the password
+        - option 2 will call the function lowercase letters
+        - option 3 will call the function uppercase letters
+        - option 4 will call the numeric function 
+        - option 5 will call the special characters function 
+  
+  5. Create the respective functions 
+        - generatePassword 
+        - passWordLenght
+        - lowerCase 
+        - upperCase
+        - numericValue
+        - specialCharacters 
+
+*/
+
+
+
+
 // Assignment code here
 
+var generatePassword = function (){
+
+}
+
+
+
 var password = function generatePassword(){
-
-
-
 }
 
 
